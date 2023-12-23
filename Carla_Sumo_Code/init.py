1fced0a97f08b62ce1f41e7a07f020cd8fc14ee3
Akua Dickson <118576545+akuadbu@users.noreply.github.com>
2023-12-22 19:31:32 -0500

    Update init.py

--- Carla_Sumo_Code/init.py
+++ Carla_Sumo_Code/init.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def init(total, range_val):
+def init():#total, range_val):
     car = {
         'cars': 0,
         'cars1': 0,
@@ -27,40 +27,40 @@ def init(total, range_val):
         'Ave_timeMerg': 0
     }
 
-    CAV_e = {
-        'acc': np.zeros((range_val, total)),
-        'arrivalexit': np.zeros((total, 2)),
-        'vel': np.zeros((range_val, total)),
-        'pos': np.zeros((range_val, total)),
-        'rear_end': np.full((range_val, total), np.nan),
-        'rear_end_CBF': np.full((range_val, total), np.nan),
-        'lateral': np.full((range_val, total, 5), np.nan),
-        'lateral_CBF': np.full((range_val, total, 5), np.nan),
-        'time': np.zeros((total, 1)),
-        'fuel': np.zeros((total, 1)),
-        'energy': np.zeros((total, 1)),
-        'x_tk': np.empty((total, 8), dtype=object),
-        'v_tk': np.empty((total, 8), dtype=object),
-        'trust_tk': np.empty((total, 6), dtype=object),
-        'posx': np.zeros((range_val, total)),
-        'posy': np.zeros((range_val, total)),
-        'angle': np.zeros((range_val, total))
-    }
+    # CAV_e = {
+    #     'acc': np.zeros((range_val, total)),
+    #     'arrivalexit': np.zeros((total, 2)),
+    #     'vel': np.zeros((range_val, total)),
+    #     'pos': np.zeros((range_val, total)),
+    #     'rear_end': np.full((range_val, total), np.nan),
+    #     'rear_end_CBF': np.full((range_val, total), np.nan),
+    #     'lateral': np.full((range_val, total, 5), np.nan),
+    #     'lateral_CBF': np.full((range_val, total, 5), np.nan),
+    #     'time': np.zeros((total, 1)),
+    #     'fuel': np.zeros((total, 1)),
+    #     'energy': np.zeros((total, 1)),
+    #     'x_tk': np.empty((total, 8), dtype=object),
+    #     'v_tk': np.empty((total, 8), dtype=object),
+    #     'trust_tk': np.empty((total, 6), dtype=object),
+    #     'posx': np.zeros((range_val, total)),
+    #     'posy': np.zeros((range_val, total)),
+    #     'angle': np.zeros((range_val, total))
+    # }
+    #
+    # for i in range(8):
+    #     for j in range(total):
+    #         CAV_e['x_tk'][j, i] = np.zeros(total)
+    #         CAV_e['v_tk'][j, i] = np.zeros(total)
+    #
+    # for i in range(6):
+    #     for j in range(total):
+    #         CAV_e['trust_tk'][j, i] = np.zeros(total)
+    #
+    # CAV_e['counter'] = np.zeros((total, 1))
+    # CAV_e['trust'] = np.zeros((range_val, total))
+    # CAV_e['buffer'] = np.zeros((total, 3))
+    # CAV_e['SpoofedCarsList'] = []
+    # CAV_e['SpoofedCAVsSuccedingcarlist'] = []
+    # CAV_e['numcollisions'] = 0
 
-    for i in range(8):
-        for j in range(total):
-            CAV_e['x_tk'][j, i] = np.zeros(total)
-            CAV_e['v_tk'][j, i] = np.zeros(total)
-
-    for i in range(6):
-        for j in range(total):
-            CAV_e['trust_tk'][j, i] = np.zeros(total)
-
-    CAV_e['counter'] = np.zeros((total, 1))
-    CAV_e['trust'] = np.zeros((range_val, total))
-    CAV_e['buffer'] = np.zeros((total, 3))
-    CAV_e['SpoofedCarsList'] = []
-    CAV_e['SpoofedCAVsSuccedingcarlist'] = []
-    CAV_e['numcollisions'] = 0
-
-    return [car, metric, CAV_e]
+    return [car, metric]#, CAV_e]
