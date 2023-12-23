23bdfd8d20cbf314caa7eafe12da187208cfe42d
EhsanSabouni <100814184+EhsanSabouni@users.noreply.github.com>
2023-12-22 20:35:11 -0500

    Changing table size

--- Carla_Sumo_Code/test/test.py
+++ Carla_Sumo_Code/test/test.py
@@ -1,36 +1,10 @@
 import unittest
+import os
+import sys
+directory = os.path.normpath(os.getcwd() + os.sep + os.pardir)
+sys.path.insert(0, directory)
+import conflictCAVS
 
-def search_for_conflictCAVS(table, egocar):
-    index = []
-    position = []
-
-    k = None
-    for i in range(len(table)):
-        if table[i][0] == egocar['id'][1]:
-            k = i
-            break
-
-    ip = -1
-    if k is not None:
-        for j in range(k - 1, 0, -1):
-            if table[j][29] == table[k][29]:
-                ip = table[j][30]
-                break
-
-        for i in range(4, len(egocar['id'])):
-            flag = 0
-            for j in range(k - 1, 0, -1):
-                if table[j][egocar['id'][i] + 1] > 0:
-                    index.append(table[j][30])
-                    position.append(table[j][egocar['id'][i] + 1])
-                    flag = 1
-                    break
-
-            if flag == 0:
-                index.append(-1)
-                position.append(-1)
-
-    return ip, index, position
 class TestSearchForConflictCAVS(unittest.TestCase):
     def test_case_1(self):
         table = [
@@ -39,11 +13,36 @@ class TestSearchForConflictCAVS(unittest.TestCase):
             [3, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 3, 3],
             [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 4]
         ]
-        egocar = {'id': [1, 'xyz', 'abc', 'def', 3, 4, 2, 8, 12]}
-        ip, index, position = search_for_conflictCAVS(table, egocar)
+        egocar = {'id': [2,1,1,3,6,4]}
+        ip, index, position = conflictCAVS.search_for_conflictCAVS(table, egocar)
         self.assertEqual(ip, -1)
-        self.assertEqual(index, [])
-        self.assertEqual(position, [])
+        self.assertEqual(index, [-1,-1])
+        self.assertEqual(position, [-1,-1])
+    def test_case_2(self):
+        table = [
+            [1, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+            [2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 0, 2, 2],
+            [3, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 3, 3],
+            [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 4]
+        ]
+        egocar = {'id': [1,2,2,2,8,9,10,11]}
+        ip, index, position = conflictCAVS.search_for_conflictCAVS(table, egocar)
+        self.assertEqual(ip, -1)
+        self.assertEqual(index, [[-1],[-1],[-1],[-1]])
+        self.assertEqual(position, [[-1],[-1],[-1],[-1]])
+    #
+    def test_case_3(self):
+        table = [
+            [1, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+            [2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 0, 2, 2],
+            [3, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 3, 3],
+            [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 4]
+        ]
+        egocar = {'id': [2,3,3,5,9,6]}
+        ip, index, position = conflictCAVS.search_for_conflictCAVS(table, egocar)
+        self.assertEqual(ip, -1)
+        self.assertEqual(index, [2,1])
+        self.assertEqual(position, [2,1])
 
 if __name__ == '__main__':
     unittest.main()
