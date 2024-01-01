fb900948c94bf0b0543085e49a2ef23115677642
anonymousCoder29 <sabbir92@bu.edu>
2024-01-01 18:26:57 -0500

    Update1

--- Carla_Sumo_Code/search_i_p.py
+++
@@ -1,8 +0,0 @@
-def search_i_p(que, new):
-    length = len(que)
-    index = None
-    for i in range(length - 1, -1, -1):
-        if que[i]['id'][2] == new['id'][2]:
-            index = i + 1
-            break
-    return index
